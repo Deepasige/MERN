85cdb65efd5c6f9423edc4cc945e321877db5687
deepasige <deepasige@gmail.com>
2026-03-09 15:32:29 +0530

    backend

---
+++ backend/routes/taskRoutes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+const {createTask} = require("../controller/taskController");
+const {protect}=require('../middleware/authmiddleware');
+
+router.use(protect);
+router.post("/create" , protect, createTask);
+module.exports = router;
